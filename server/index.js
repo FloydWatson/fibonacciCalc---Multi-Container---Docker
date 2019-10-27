fde3856d58ef49b363bbc9178c350a5a7fbd9dd9
FloydWatson <floydbcseit@gmail.com>
2019-10-27 16:25:10 +1300

    pg setup

--- server/index.js
+++ server/index.js
@@ -9,4 +9,18 @@ const app = express(); // create new express application
 app.use(cors()); // cross origin resource sharing. allows us to make requests across domains
 app.use(bodyParser.json()); // parse incoming requests to json values
 
-// 
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
+});
+pgClient.on('error', () => console.log('Lost PG Connection'));
+
+// initial db setup
+pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)') 
+    .catch((err) => console.log(err));
